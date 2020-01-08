67e947102d77062be7b88b34eb57708f4a074b8b
WILL_HARRIS <wharris64@ivytech.edu>
2020-01-08 10:55:29 -0500

    hid secret key in env, added .env to gitignore

--- GameFaqs_Backend/settings.py
+++ GameFaqs_Backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # from GameFaqs_Backend import user
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -21,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2prm-@6&7$@g2wngs3xy1i^dk!hro!bvodf8@z(kw3517=2dka'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
