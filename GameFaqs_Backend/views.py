ea2d198e59eed0a69e0cd95e978407feb68952cd
WILL_HARRIS <wharris64@ivytech.edu>
2020-01-09 13:55:50 -0500

    views for adding faq and adding message

--- GameFaqs_Backend/views.py
+++ GameFaqs_Backend/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect, reverse
 from GameFaqs_Backend import models
 from GameFaqs_Backend import forms
 from django.views import View
+from
 
 
 class ViewMainPage(View):
@@ -31,3 +32,45 @@ class ViewFaqs(View):
         html = 'faqs.html'
         data = models.Faq.objects.filter(id=id)
         return render(request, html, {'data': data})
+@login_required
+class AddFaqView(request, id):
+    form = None
+    html = "addfaq.html"
+    if request.method =="POST":
+        form = Add_FAQ(request.POST)
+
+        if form.is_valid()
+            data= form.cleaned_data
+            Faq.objects.create(
+            user=request.user,
+            name=data['name'],
+            body=data['body'],
+            game=game.objects.get.filter.(id=id).first()
+            )
+        return redirect('/')
+        else:
+            return HttpResponseRedirect(reverse('/'))
+    else:
+        form = Add_FAQ()
+    return render(request, html,{'form':form})
+
+class AddMessageView(request, id):
+    form = None
+    html = addmessage.html
+    if request.method == "POST":
+        form = Add_Message(request.POST)
+
+        if form.is_valid()
+        data= form.cleaned_data
+        Message.objects.create(
+        user=request.user,
+        title=data['title'],
+        body=data['body'],
+        game=game.objects.get.filter.(id=id).first()
+        )
+        return redirect('/')
+        else:
+            return HttpResponseRedirect(reverse('/'))
+    else:
+        form = Add_Message()
+    return render(request, html, {'form':form})
