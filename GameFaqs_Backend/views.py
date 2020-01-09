a20f6bc92969e8d0ad6ec600df83c79778d99ba6
Shaquon <shaquonkelley@gmail.com>
2020-01-09 12:36:30 -0500

    made login and register forms, views, and generic template

--- GameFaqs_Backend/views.py
+++ GameFaqs_Backend/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from GameFaqs_Backend import models
-
+from GameFaqs_Backend.forms import LoginForm, RegisterForm
+from GameFaqs_Backend.models import GFUser
 
 def login_view(request):
-    
-    html = 'generic_form.htm'
+    html = 'generic_form.html'
     page = 'login'
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -21,4 +21,34 @@ def login_view(request):
             )
 
     form = LoginForm()
+    return render(request, html, {'form': form, 'page': page})
    
+
+def register_user_view(request):
+    html = 'generic_form.html'
+    page = 'register'
+    if request.method == "POST":
+
+        form = RegisterForm(request.POST)
+
+        # already_a_user = User.objects.filter(username=current_user)
+
+        # if already_a_user.exists():
+        #     raise form.ValidationError("That user is already taken")
+
+        if form.is_valid():
+
+            data = form.cleaned_data
+
+            u = User.objects.create_user(
+                username=data['username'],
+                password=data['password'])
+
+            GFUser.objects.create(user=u)
+
+            login(request, u)
+            return HttpResponseRedirect(reverse('home'))
+    form = RegisterForm()
+
+    return render(request, html, {'form': form, 'page': page})
+
