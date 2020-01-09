a5f516c0b6871ea7c81ead1614faa86f686ae15c
Shaquon <shaquonkelley@gmail.com>
2020-01-09 12:12:56 -0500

    fresh start

--- GameFaqs_Backend/views.py
+++ GameFaqs_Backend/views.py
@@ -1,2 +1,24 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
 from GameFaqs_Backend import models
+
+
+def login_view(request):
+    
+    html = 'generic_form.htm'
+    page = 'login'
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                username=data['username'],
+                password=data['password']
+            )
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(
+                request.GET.get('next', reverse('home'))
+            )
+
+    form = LoginForm()
+   
